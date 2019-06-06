6dd1cab8ff84854711fa8e06b223e027a25928fa
JuanArmas <43064632+JuanArmas@users.noreply.github.com>
2019-06-06 09:40:56 +0100

    segunda version con varios ejercicios finalizados

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,33 +1,41 @@
 package controller;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
 import model.Cuenta;
-
+import model.Movimiento;
 
 
 public class Main {
 
 	public static void main(String[] args) {
+		Main ejercicios = new Main();
 		String rutaVehiculos = "ficheros/vehiculos.csv";
 		
 		/*******************EJERCICIO 1******************************/
-		
-		
+		// 1.1
+			ejercicios.insertaVehiculos(rutaVehiculos);
 		
 		/*******************EJERCICIO 2******************************/
-		
-		
-		
+		// 2.1-
+			// HashMap<Integer, Cuenta> mapaCuentas = ejercicios.cuentas();
+			// System.out.println(mapaCuentas);
+		//2.2-
+			// HashMap<Cuenta, ArrayList<Movimiento>> movimientos = ejercicios.listadoMovimientos();
+			// System.out.println(movimientos);
+		//2.3-
+			
 		/*******************EJERCICIO 3******************************/
 		
 		
@@ -56,8 +64,8 @@ public class Main {
 				float precio = Float.parseFloat(campos[4]);
 				String fechaMatricula = campos[5];
 				
-				String sql = "insert into equipos(codigo,matricula,fecha,estado,precio,nif) values";
-				sql+= "(" + codigo + "\"" + matricula + "\",\"" + fechaMatricula + "\",\"" + estado + "\","+ precio + "\"" + nifPropietario + "\")";			
+				String sql = "insert into vehiculos(codigo,matricula,fecha,estado,precio,nif) values";
+				sql+= "(" + codigo + ",\"" + matricula + "\",\"" + fechaMatricula + "\",\"" + estado + "\","+ precio + ",\"" + nifPropietario + "\")";			
 				System.out.println(sql);	
 				stmt.executeUpdate(sql);				
 			}	
@@ -78,23 +86,108 @@ public class Main {
 	
 	
 	// 2.1 Desarrollar un método que devuelva un objeto de la clase
-	
+	// id, descripcion,saldo
 	public HashMap <Integer,Cuenta> cuentas(){
 		HashMap <Integer, Cuenta> cuentas = new HashMap <Integer, Cuenta>();
-		
-		
-		return cuentas;
+		try {
+			BaseDatos bd = new BaseDatos("localhost:3306",  "banco", "root", "");
+			Connection conexion = bd.getConexion();
+			Statement stmt = conexion.createStatement();
+			ResultSet rst = stmt.executeQuery("select * from cuentas where 1;");
+			ResultSetMetaData rstMeta = rst.getMetaData();
+			rstMeta.getColumnCount(); 
+			// id, String descripcion, float saldo
+			while(rst.next()) { 		
+				int id = Integer.parseInt(rst.getString(1));
+				String descripcion = rst.getString(2);
+				float saldo = Float.parseFloat(rst.getString(3));
+				Cuenta cuenta = new Cuenta(id, descripcion, saldo);
+				cuentas.put(id, cuenta);			
+			}
+			rst.close();
+			stmt.close();
+			conexion.close();
+			return cuentas;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}catch (NullPointerException e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
 	}
 	
+	// 2.2. Desarrollar un método que devuelva un objeto de la clase
 	
+	public HashMap < Cuenta, ArrayList<Movimiento>> listadoMovimientos(){
+		Main ejercicios = new Main();
+		HashMap<Integer, Cuenta> cuentas = ejercicios.cuentas();
+		HashMap < Cuenta, ArrayList<Movimiento>> listaMovs= new HashMap < Cuenta, ArrayList<Movimiento>>();
+		try {
+			BaseDatos bd = new BaseDatos("localhost:3306",  "banco", "root", "");
+			Connection conexion = bd.getConexion();
+			Statement stmt = conexion.createStatement();
+			ResultSet rst = stmt.executeQuery("select * from movs where 1;");
+			ResultSetMetaData rstMeta = rst.getMetaData();
+			rstMeta.getColumnCount(); 
+			// id, int idcta, String fecha, String descripcion, float importe
+			while(rst.next()) { 		
+				int id = Integer.parseInt(rst.getString(1));
+				int idcta = Integer.parseInt(rst.getString(2));
+				String fecha = rst.getString(3);
+				String descripcion = rst.getString(4);
+				float importe = Float.parseFloat(rst.getString(5));			
+				Movimiento movimientos = new Movimiento(id, idcta, fecha, descripcion, importe);
+				
+				Set<Integer> clavesCuentas = cuentas.keySet();
+				for(Integer clave:clavesCuentas) {
+					Cuenta unaCuenta = cuentas.get(clave);
+					ArrayList<Movimiento> movimientoCuenta = new ArrayList<Movimiento>();
+					movimientoCuenta.add(movimientos);
+					listaMovs.put(unaCuenta, movimientoCuenta);
+				}						
+			}
+			rst.close();
+			stmt.close();
+			conexion.close();
+			return listaMovs;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}catch (NullPointerException e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
 	
-	
-	
-	
-	
-	
-	
-	
+	// 2.3-
+	public void mostrarMovimientosCuenta() {
+		Main ejercicios = new Main();
+		HashMap<Integer, Cuenta> mapaCuentas = ejercicios.cuentas();
+		HashMap<Cuenta, ArrayList<Movimiento>> movimientos = ejercicios.listadoMovimientos();
+		System.out.println("LISTADO DE MOVIMIENTOS A CUENTAS...");
+		
+		Set<Integer> clavesCuentas = mapaCuentas.keySet();
+		Set<Cuenta> clavesMovimientos = movimientos.keySet();	
+		for( Integer numCuenta : clavesCuentas) {
+			Cuenta saldoCuenta = mapaCuentas.get(numCuenta);
+			for(Cuenta numCuentaMovimiento: clavesMovimientos) {
+				 ArrayList<Movimiento> listaMovimientos = movimientos.get(numCuentaMovimiento);
+				 
+				 if(numCuentaMovimiento.equals(numCuenta)) {
+					System.out.println("Cuenta :" + "cuenta"+numCuenta);					
+					System.out.println("Saldo Inicial :" + saldoCuenta );	
+					
+					for(int i = 0; i < listaMovimientos.size(); i++) {	
+
+						
+						// float saldoFinal = saldoCuenta + listaMovimientos.get(i);
+						System.out.println("\t\t" + listaMovimientos.get(i));
+						// System.out.println("Saldo Final : \t" + saldoFinal);						
+					}
+				}
+			}
+		}
+		
+	}
 	
 	
 }
--- src/model/Movimiento.java
+++ src/model/Movimiento.java
@@ -8,4 +8,63 @@ public class Movimiento {
 	private String descripcion;
 	private float importe;
 	
+	public Movimiento() {
+		super();
+	}
+
+	public Movimiento(int id, int idcta, String fecha, String descripcion, float importe) {
+		super();
+		this.id = id;
+		this.idcta = idcta;
+		this.fecha = fecha;
+		this.descripcion = descripcion;
+		this.importe = importe;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getIdcta() {
+		return idcta;
+	}
+
+	public void setIdcta(int idcta) {
+		this.idcta = idcta;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public float getImporte() {
+		return importe;
+	}
+
+	public void setImporte(float importe) {
+		this.importe = importe;
+	}
+
+	@Override
+	public String toString() {
+		return "Movimiento [id=" + id + ", idcta=" + idcta + ", fecha=" + fecha + ", descripcion=" + descripcion
+				+ ", importe=" + importe + "]";
+	}
+	
 }
